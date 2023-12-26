e6f54e0a98e90b0a43cc56e94061c857da534339
Shishir Shrivastava <140878026+Shishir2405@users.noreply.github.com>
2023-12-26 11:35:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 let play = document.querySelector(".play");
 let audio = document.querySelector(".center");
+let next = document.querySelector(".next");
 play.addEventListener("click", function () {
+  next.pause();
   audio.play();
   console.log("play");
 });
-let next = document.querySelector(".next");
 let nextBtn = document.querySelector(".nextBtn");
 nextBtn.addEventListener("click", function () {
+  audio.pause();
   next.play();
   console.log("play");
 });
